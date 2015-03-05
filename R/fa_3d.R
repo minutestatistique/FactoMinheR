285a29fde0450b28245b1e7c9a17c63e3bfd28d6
minutestatistique <minh@adikteev.com>
2015-03-05 10:31:20 +0100

    add sphere for the PCA variables plot

--- R/fa_3d.R
+++ R/fa_3d.R
@@ -6,6 +6,7 @@
 #' @param dim the three dimensions to plot.
 #' @param grps list of rownames of different group(s).
 #' @param colors colors of different group(s).
+#' @param alpha transparence for the PCA variables sphere.
 #' @return Nothing. Used for side-effects.
 #' @keywords 3D
 #' @export
@@ -14,7 +15,7 @@
 #' data(decathlon)
 #' res.pca = PCA(decathlon[, 1:10], scale.unit = TRUE, ncp = 5, graph = TRUE)
 #' plot_fa_3d(res.pca)
-plot_fa_3d <- function(x, dim = 1:3, grps = NULL, colors = rainbow(length(grps))) {
+plot_fa_3d <- function(x, dim = 1:3, grps = NULL, colors = rainbow(length(grps)), alpha = 0.1) {
   if (!requireNamespace("rgl", quietly = TRUE)) {
     stop("rgl needed for this function to work. Please install it.",
          call. = FALSE)
@@ -31,9 +32,13 @@ plot_fa_3d <- function(x, dim = 1:3, grps = NULL, colors = rainbow(length(grps))
   if (inherits(x, "MCA") || inherits(x, "PCA")) {
     my.coord.row <- x$ind$coord
     my.coord.col <- x$var$coord
+    my.main.row <- "inds"
+    my.main.col <- "vars"
   } else if (inherits(x, "CA")) {
     my.coord.row <- x$row$coord
     my.coord.col <- x$col$coord
+    my.main.row <- "rows"
+    my.main.col <- "cols"
   } else {
     stop("Analysis not supported.")
   }
@@ -66,7 +71,7 @@ plot_fa_3d <- function(x, dim = 1:3, grps = NULL, colors = rainbow(length(grps))
                   zlab = colnames(my.coord.row)[my.z.dim],
                   xlim = my.x.lim, ylim = my.y.lim, zlim = my.z.lim,
                   col = colors[i],
-                  main = "rows")
+                  main = my.main.row)
     } else {
       rgl::points3d(my.coord.row[grps[[i]], my.x.dim],
                     my.coord.row[grps[[i]], my.y.dim],
@@ -76,15 +81,27 @@ plot_fa_3d <- function(x, dim = 1:3, grps = NULL, colors = rainbow(length(grps))
   }
 
   # plot cols
-  # TODO plot sphere when PCA
   rgl::open3d()
-  rgl::plot3d(x = my.coord.col[, my.x.dim],
-              y = my.coord.col[, my.y.dim],
-              z = my.coord.col[, my.z.dim],
-              xlab = colnames(my.coord.col)[my.x.dim],
-              ylab = colnames(my.coord.col)[my.y.dim],
-              zlab = colnames(my.coord.col)[my.z.dim],
-              main = "cols")
+  if (inherits(x, "PCA")) {
+    rgl::plot3d(x = my.coord.col[, my.x.dim],
+                y = my.coord.col[, my.y.dim],
+                z = my.coord.col[, my.z.dim],
+                xlab = colnames(my.coord.col)[my.x.dim],
+                ylab = colnames(my.coord.col)[my.y.dim],
+                zlab = colnames(my.coord.col)[my.z.dim],
+                xlim = c(-1, 1), ylim = c(-1, 1), zlim = c(-1, 1),
+                main = my.main.col)
+    rgl::spheres3d(x = 0, y = 0, z = 0, radius = 1, color = "grey",
+                   alpha = alpha, smooth = TRUE)
+  } else {
+    rgl::plot3d(x = my.coord.col[, my.x.dim],
+                y = my.coord.col[, my.y.dim],
+                z = my.coord.col[, my.z.dim],
+                xlab = colnames(my.coord.col)[my.x.dim],
+                ylab = colnames(my.coord.col)[my.y.dim],
+                zlab = colnames(my.coord.col)[my.z.dim],
+                main = my.main.col)
+  }
   rgl::text3d(x = my.coord.col[, my.x.dim],
               y = my.coord.col[, my.y.dim],
               z = my.coord.col[, my.z.dim],
